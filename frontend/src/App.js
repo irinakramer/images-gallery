0a3d494d76467142146c669209d28ae8cbac1516
Irina Kramer <irakramer@gmail.com>
2023-05-01 12:19:38 -0700

    added images cards

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,14 +2,14 @@ import { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
 import Search from './components/Search';
+import ImageCard from './components/ImageCard';
+import { Container, Row, Col } from 'react-bootstrap';
 
 const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
 
 const App = () => {
   const [word, setWord] = useState('');
   const [images, setImages] = useState([]);
-
-  console.log(images);
 
   const handleSearchSubmit = (e) => {
     e.preventDefault();
@@ -20,7 +20,7 @@ const App = () => {
     )
       .then((res) => res.json())
       .then((data) => {
-        setImages([data, ...images]); // new array of images including new and previous
+        setImages([{ ...data, title: word }, ...images]); // new array of images including new and previous
       })
       .catch((err) => {
         console.log(err);
@@ -33,6 +33,15 @@ const App = () => {
     <div className="App">
       <Header title="Images Gallery" />
       <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit} />
+      <Container className="mt-4">
+        <Row xs={1} md={2} lg={3}>
+          {images.map((image, i) => (
+            <Col key={i} className="pb-3">
+              <ImageCard image={image} />
+            </Col>
+          ))}
+        </Row>
+      </Container>
     </div>
   );
 };
